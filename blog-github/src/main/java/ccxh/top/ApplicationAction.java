edb746dcd85ad055f88812057da9fd51413773bb
sunjiaqing <sundada214@outlook.com>
2018-08-31 20:42:55 +0800

    更改之前阻塞线程的做法 使用新的方式让线程不退出

--- blog-github/src/main/java/ccxh/top/ApplicationAction.java
+++ blog-github/src/main/java/ccxh/top/ApplicationAction.java
@@ -4,6 +4,7 @@ import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import tk.mybatis.spring.annotation.MapperScan;
 
 import java.util.concurrent.CountDownLatch;
@@ -15,18 +16,10 @@ import java.util.concurrent.CountDownLatch;
 @SpringBootApplication
 @DubboComponentScan(basePackages = "ccxh.top.service.impl")
 @MapperScan("ccxh.top.blog.github.mapper")
-public class ApplicationAction implements CommandLineRunner {
+public class ApplicationAction {
     public static void main(String[] args) {
-        SpringApplication.run(ApplicationAction.class, args);
+        //修改为不占用端口启动方法
+        new SpringApplicationBuilder(ApplicationAction .class).web(false).run(args);
     }
 
-    @Override
-    public void run(String... strings) throws Exception {
-        CountDownLatch countDownLatch = new CountDownLatch(1);
-        //会影响测试
-        while (countDownLatch!=null){
-            countDownLatch.await();
-            System.out.println("await");
-        }
-    }
 }
