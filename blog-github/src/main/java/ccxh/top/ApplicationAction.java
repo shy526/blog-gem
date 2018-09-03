e16dee82a513fb955d6dc049f453ca006e1a4171
sunjiaqing <sundada214@outlook.com>
2018-09-03 11:22:17 +0800

    更新配置文件 并 添加一个项目启动初始化操作

--- blog-github/src/main/java/ccxh/top/ApplicationAction.java
+++ blog-github/src/main/java/ccxh/top/ApplicationAction.java
@@ -4,7 +4,6 @@ import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import tk.mybatis.spring.annotation.MapperScan;
 
 import java.util.concurrent.CountDownLatch;
@@ -16,10 +15,17 @@ import java.util.concurrent.CountDownLatch;
 @SpringBootApplication
 @DubboComponentScan(basePackages = "ccxh.top.service.impl")
 @MapperScan("ccxh.top.blog.github.mapper")
-public class ApplicationAction {
+public class ApplicationAction implements CommandLineRunner {
     public static void main(String[] args) {
-        //修改为不占用端口启动方法
-        new SpringApplicationBuilder(ApplicationAction .class).web(false).run(args);
+        SpringApplication.run(ApplicationAction.class, args);
     }
 
+    @Override
+    public void run(String... strings) throws Exception {
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        while (countDownLatch!=null){
+            countDownLatch.await();
+            Thread.sleep(10000);
+        }
+    }
 }
--- blog-schedule/src/main/java/ccxh/top/schedule/ApplicationRun.java
+++ blog-schedule/src/main/java/ccxh/top/schedule/ApplicationRun.java
@@ -1,6 +1,8 @@
 package ccxh.top.schedule;
 
 import ccxh.top.schedule.task.impl.GithubTask;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -10,11 +12,13 @@ import org.springframework.boot.ApplicationRunner;
  * @author admin
  */
 public class ApplicationRun implements ApplicationRunner {
+    private final static Logger LOGGER= LoggerFactory.getLogger(ApplicationRun.class);
     @Autowired
     GithubTask githubTask;
     @Override
     public void run(ApplicationArguments applicationArguments) throws Exception {
         //启动后 查看列表
         githubTask.dispatch();
+        LOGGER.info("init project");
     }
 }
